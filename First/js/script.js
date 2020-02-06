97185690ebbec707d9a3178c3f4ee238f1b08ec6
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-06 17:08:23 +0600

    disable re-rendering using v-once directive

--- First/js/script.js
+++ First/js/script.js
@@ -1,19 +1,21 @@
-// var app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Hello Vue!',
-//     link: 'https://google.com'
-//   },
-//   methods: {
-//     changeMessage: function(event) {
-//       return this.message;
-//     }
-//   }
-// });
-
-var app2 = new Vue({
-  el: '#app-2',
+var app = new Vue({
+  el: '#app',
   data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
+    message: 'Hello Vue!',
+    link: 'https://google.com'
+  },
+  methods: {
+    changeMessage: function(event) {
+      this.message = 'Hello!';
+      return this.message;
+    }
   }
 });
+
+// dynamically bounding title on hovering
+// var app2 = new Vue({
+//   el: '#app-2',
+//   data: {
+//     message: 'You loaded this page on ' + new Date().toLocaleString()
+//   }
+// });
