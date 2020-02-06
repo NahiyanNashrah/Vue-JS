7d31fcf7ecffccd6a2a9c5398b474b8e583be307
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-06 14:27:10 +0600

    a simple directive example

--- First/js/script.js
+++ First/js/script.js
@@ -2,6 +2,11 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
+  },
+  methods: {
+    changeMessage: function(event) {
+      this.message = event.target.value;
+    }
   }
 });
 app.message = 'data has changed!';
