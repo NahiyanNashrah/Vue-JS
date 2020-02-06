e3d7807ffacc63c69a62bdc912c5d04d87aaf321
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-06 16:56:19 +0600

    binding title dynamically

--- First/js/script.js
+++ First/js/script.js
@@ -1,12 +1,19 @@
-var app = new Vue({
-  el: '#app',
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello Vue!',
+//     link: 'https://google.com'
+//   },
+//   methods: {
+//     changeMessage: function(event) {
+//       return this.message;
+//     }
+//   }
+// });
+
+var app2 = new Vue({
+  el: '#app-2',
   data: {
-    message: 'Hello Vue!',
-    link: 'https://google.com'
-  },
-  methods: {
-    changeMessage: function(event) {
-      return this.message;
-    }
+    message: 'You loaded this page on ' + new Date().toLocaleString()
   }
 });
