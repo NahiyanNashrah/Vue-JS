eaacf4dd48c561e00e275d34acfac764290077fd
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-08 23:39:29 +0600

    Attribute binding

--- Product page/js/script.js
+++ Product page/js/script.js
@@ -4,6 +4,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks'
+    // description: 'A pair of warm, fuzzy socks'
+    image: './vmSocks-green-onWhite.jpg'
   }
 });
