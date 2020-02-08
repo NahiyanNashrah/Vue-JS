d54299ba4c59b271f303255b83a656873fb9f006
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-08 23:57:19 +0600

    Conditional Rendering

--- Product page/js/script.js
+++ Product page/js/script.js
@@ -5,6 +5,7 @@ var app = new Vue({
   data: {
     product: 'Socks',
     // description: 'A pair of warm, fuzzy socks'
-    image: './vmSocks-green-onWhite.jpg'
+    image: './vmSocks-green-onWhite.jpg',
+    inventory: '10'
   }
 });
