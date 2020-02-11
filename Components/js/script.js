4cd3269a69ae5fdcab4fecaef6fac4b3ff2bc03e
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-11 18:59:54 +0600

    component registration

--- Components/js/script.js
+++ Components/js/script.js
@@ -1,19 +1,23 @@
-Vue.component('plan-picker', {
-  template: '#plan-picker-template',
-  data() {
-    return {
-      plans: ['The Single', 'The Curious', 'The Addict']
-    };
-  }
-});
-Vue.component('plan', {
+let PlanComponent = {
   template: '#plan-template',
   props: {
     name: {
       type: String
     }
   }
+};
+Vue.component('plan-picker', {
+  template: '#plan-picker-template',
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    };
+  },
+  components: {
+    plan: PlanComponent
+  }
 });
+
 new Vue({
   el: '#app'
 });
