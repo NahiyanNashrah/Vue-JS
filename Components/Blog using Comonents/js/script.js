44eb2772ca2d3e93adaddaec261170447b8e8242
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-12 18:41:07 +0600

    simple changes

---
+++ Components/Blog using Comonents/js/script.js
@@ -0,0 +1,19 @@
+Vue.component('blog-post', {
+  props: {
+    attr: {
+      type: Object
+    }
+  },
+  template: `#blog-post-template`
+});
+new Vue({
+  el: '#blog-post-demo',
+  data: {
+    posts: [
+      { id: 1, title: 'My journey with Vue', content: 'First post' },
+      { id: 2, title: 'Blogging with Vue', content: 'Second post' },
+      { id: 3, title: 'Why Vue is so fun', content: 'Third post' }
+    ],
+    fontSize: 14
+  }
+});
