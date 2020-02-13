80472219c4793fc1d9db8e79344fead3ccc475b7
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-13 12:36:55 +0600

    create componet with vue-cli

---
+++ vue-cli/src/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Server status: {{ status }}</h2>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: "critical"
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = "normal";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
