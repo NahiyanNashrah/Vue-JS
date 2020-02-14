92d4761ce3364cb3fc9de99107977826aa665c5c
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-14 14:01:09 +0600

    components with vue cli

---
+++ vue-cli/src/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="servers">
+    <server-status v-for="server in 5" :key="server.id"></server-status>
+  </div>
+</template>
+
+<script>
+import ServerStatus from "./ServerStatus.vue";
+export default {
+  components: {
+    "server-status": ServerStatus
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-cli/src/ServerStatus.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="server-status">
+    <h3>Server status: {{status}}</h3>
+    <button @click="changeStatus">Change Status</button>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: "critical"
+    };
+  },
+  methods: {
+    changeStatus() {
+      this.status = "normal";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
