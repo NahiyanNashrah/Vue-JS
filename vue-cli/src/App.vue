096e8c0cb7dfec9317b12429582ab73734699b48
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-15 12:50:05 +0600

    sturcturing components

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,44 +1,30 @@
 <template>
-  <app-server></app-server>
+  <div class="container">
+    <app-header></app-header>
+    <hr />
+    <div class="row">
+      <servers></servers>
+      <app-server-details></app-server-details>
+    </div>
+    <hr />
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Shared/Header.vue";
+import Footer from "./components/Shared/Footer.vue";
+import Servers from "./components/Server/Servers.vue";
+import ServerDetails from "./components/Server/ServerDetails.vue";
+
 export default {
-  name: "app",
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
+  components: {
+    appHeader: Header,
+    Servers,
+    "app-server-details": ServerDetails,
+    "app-footer": Footer
   }
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
