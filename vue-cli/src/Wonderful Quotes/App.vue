11b791e0733a71a61f3b1e55bd600889f87f8a62
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-17 13:23:51 +0600

    adding quotes & showing it

--- vue-cli/src/Wonderful Quotes/App.vue
+++ vue-cli/src/Wonderful Quotes/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container">
+    <app-new-quote @addQuote="showQuote($event,)"></app-new-quote>
     <app-quote-grid :quotes="quotesArr"></app-quote-grid>
   </div>
 </template>
 
 <script>
 import QuoteGrid from "../Wonderful Quotes/components/QuoteGrid";
+import NewQuote from "../Wonderful Quotes/components/NewQuote";
+
 export default {
   data() {
     return {
@@ -14,7 +17,13 @@ export default {
     };
   },
   components: {
-    "app-quote-grid": QuoteGrid
+    "app-quote-grid": QuoteGrid,
+    "app-new-quote": NewQuote
+  },
+  methods: {
+    showQuote(quote) {
+      this.quotesArr.push(quote);
+    }
   }
 };
 </script>
