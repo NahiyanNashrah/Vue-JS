3a0d29c19f15a6f74a34c5d50daaa9c3ad3419e4
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-18 19:06:16 +0600

    passing values to custom directives

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -5,12 +5,12 @@ import App from "./Creating directives/App.vue";
 // //import App from "./advancedComponentUsage/App.vue";
 // import App from "./App.vue";
 // export const eventBus = new Vue();
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = binding.value;
+  }
+});
 new Vue({
   el: "#app",
   render: h => h(App)
-});
-Vue.directive("highlight", {
-  bind(el, binding, vnode) {
-    el.style.background = "green";
-  }
 });
