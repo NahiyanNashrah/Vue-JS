cdd675eac50e74ef6bc20472e0d46d06e3c9e2fe
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-19 11:59:57 +0600

    adding modifier

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -9,14 +9,16 @@ Vue.directive("highlight", {
   bind(el, binding, vnode) {
     var delay = 0;
 
-    if (binding.arg == "background") {
+    if (binding.modifiers["delayed"]) {
       delay = 2500;
-      setTimeout(() => {
-        el.style.backgroundColor = binding.value;
-      }, delay);
-    } else {
-      el.style.color = binding.value;
     }
+    setTimeout(() => {
+      if (binding.arg == "background") {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 new Vue({
