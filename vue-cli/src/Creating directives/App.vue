24d4b0b5476a60933c63e2b2bce8ba096169bd76
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-19 12:06:27 +0600

    registering a component locally

--- vue-cli/src/Creating directives/App.vue
+++ vue-cli/src/Creating directives/App.vue
@@ -12,13 +12,35 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom Directives</h1>
         <p v-highlight:background.delayed="'blue'">Color this text using custom directive</p>
+        <p
+          v-local-highlight:background.delayed="'yellow'"
+        >Color this text using locally registered directive</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+
+        if (binding.modifiers["delayed"]) {
+          delay = 3500;
+        }
+        setTimeout(() => {
+          if (binding.arg == "background") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, delay);
+      }
+    }
+  }
+};
 </script>
 
 <style>
