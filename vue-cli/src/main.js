d069f9a6077888825d4b77fde0fb461091130f7f
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-19 11:25:53 +0600

    passing argumnets to custom directives

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -7,7 +7,16 @@ import App from "./Creating directives/App.vue";
 // export const eventBus = new Vue();
 Vue.directive("highlight", {
   bind(el, binding, vnode) {
-    el.style.backgroundColor = binding.value;
+    var delay = 0;
+
+    if (binding.arg == "background") {
+      delay = 2500;
+      setTimeout(() => {
+        el.style.backgroundColor = binding.value;
+      }, delay);
+    } else {
+      el.style.color = binding.value;
+    }
   }
 });
 new Vue({
