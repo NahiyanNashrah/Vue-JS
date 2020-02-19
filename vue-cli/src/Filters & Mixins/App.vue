9148d5050109906efe0ead974be009d270f193e2
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-19 15:42:56 +0600

    filter with computed properties

---
+++ vue-cli/src/Filters & Mixins/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{text | toUpper }}</p>
+      </div>
+    </div>
+    <hr />
+    <input type="text" v-model="filteredText" />
+    <ul>
+      <li v-for="fruit in filteredFruits" :key="fruit.id">{{fruit}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Filters & Mixins",
+      fruits: ["Apple", "Banana", "Grape", "PineApple", "Mango", "Melon"],
+      filteredText: ""
+    };
+  },
+  filters: {
+    toUpper(value) {
+      return value.toUpperCase();
+    }
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter(item => {
+        return item.match(this.filteredText);
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,44 +1,45 @@
 import Vue from "vue";
-import App from "./Creating directives/App.vue";
+import App from "./Filters & Mixins/App.vue";
+// import App from "./Creating directives/App.vue";
 // import App from "./Handling User Input with Forms/App.vue";
-// //import App from "./Wonderful Quotes/App.vue";
+// import App from "./Wonderful Quotes/App.vue";
 // //import App from "./advancedComponentUsage/App.vue";
 // import App from "./App.vue";
 // export const eventBus = new Vue();
-Vue.directive("highlight", {
-  bind(el, binding, vnode) {
-    var delay = 0;
+// Vue.directive("highlight", {
+//   bind(el, binding, vnode) {
+//     var delay = 0;
 
-    if (binding.modifiers["delayed"]) {
-      delay = 2500;
-    }
-    if (binding.modifiers["another"]) {
-      el.style.fontSize = "24px";
-      let style1 = "italic";
-      let style2 = "normal";
-      let current = style1;
-      setTimeout(() => {
-        setInterval(() => {
-          current == style1 ? (current = style2) : (current = style1);
-          el.style.fontStyle = current;
-          if (binding.arg == "background") {
-            el.style.backgroundColor = binding.value;
-          } else {
-            el.style.color = binding.value;
-          }
-        }, 1000);
-      }, delay);
-    } else {
-      setTimeout(() => {
-        if (binding.arg == "background") {
-          el.style.backgroundColor = binding.value;
-        } else {
-          el.style.color = binding.value;
-        }
-      }, delay);
-    }
-  }
-});
+//     if (binding.modifiers["delayed"]) {
+//       delay = 2500;
+//     }
+//     if (binding.modifiers["another"]) {
+//       el.style.fontSize = "24px";
+//       let style1 = "italic";
+//       let style2 = "normal";
+//       let current = style1;
+//       setTimeout(() => {
+//         setInterval(() => {
+//           current == style1 ? (current = style2) : (current = style1);
+//           el.style.fontStyle = current;
+//           if (binding.arg == "background") {
+//             el.style.backgroundColor = binding.value;
+//           } else {
+//             el.style.color = binding.value;
+//           }
+//         }, 1000);
+//       }, delay);
+//     } else {
+//       setTimeout(() => {
+//         if (binding.arg == "background") {
+//           el.style.backgroundColor = binding.value;
+//         } else {
+//           el.style.color = binding.value;
+//         }
+//       }, delay);
+//     }
+//   }
+// });
 new Vue({
   el: "#app",
   render: h => h(App)
