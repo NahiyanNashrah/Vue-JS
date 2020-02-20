295b5d5d6de3543275282c624d237515af8175a7
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-20 17:20:48 +0600

    custom axios instnces

---
+++ vue-cli/src/Axios/axios-auth.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://axios-2ae55.firebaseio.com"
+});
+instance.defaults.headers.common["Something"] = "something";
+export default instance;
