db0753ccba3f380525de9833a425140106a8a9d7
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-23 16:02:49 +0600

    fetching & using router parameters

--- vue-cli/src/Routing/components/Header.vue
+++ vue-cli/src/Routing/components/Header.vue
@@ -4,7 +4,7 @@
       ><a>Home</a></router-link
     >
 
-    <router-link to="/user" tag="li" active-class="active"
+    <router-link to="/user/1" tag="li" active-class="active"
       ><a>User</a></router-link
     >
   </ul>
--- vue-cli/src/Routing/components/user/User.vue
+++ vue-cli/src/Routing/components/user/User.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>The User Page</h1>
+  <div class="User">
+    <h1>The User Page</h1>
+    <hr />
+    <p>Loaded id: {{ id }}</p>
+    <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    };
+  },
+  methods: {
+    navigateToHome() {
+      this.$router.push("/");
+    }
+  }
+};
+</script>
