7f4a890f2220a91d764ce7e9be9416a00564db08
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-23 15:02:09 +0600

    navigating with router link

---
+++ vue-cli/src/Routing/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="template">
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/user">User</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
