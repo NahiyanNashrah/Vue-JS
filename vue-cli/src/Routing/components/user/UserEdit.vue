667e42aa5e4433d1bdf722dd947aa6f1701de6e6
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-24 15:49:17 +0600

    beforeleave guard

--- vue-cli/src/Routing/components/user/UserEdit.vue
+++ vue-cli/src/Routing/components/user/UserEdit.vue
@@ -3,7 +3,28 @@
     <h3>Edit the User</h3>
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
+    <button class="btn btn-primary" @click="confirmed=true">Confirm</button>
     <div style="height:700px"></div>
     <p id="data">Some Information</p>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      confirmed: false
+    };
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.confirmed) {
+      next();
+    } else {
+      if (confirm("Are you sure?")) {
+        next();
+      } else {
+        next(false);
+      }
+    }
+  }
+};
+</script>
