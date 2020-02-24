41d7c571c819c74972d37875694aa1431395c511
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-24 12:07:20 +0600

    named router views

--- vue-cli/src/Routing/router.js
+++ vue-cli/src/Routing/router.js
@@ -2,14 +2,23 @@ import User from "./components/user/User.vue";
 import UserDetail from "./components/user/UserDetail.vue";
 import UserStart from "./components/user/UserStart.vue";
 import UserEdit from "./components/user/UserEdit.vue";
-
+import Header from "./components/Header.vue";
 import Home from "./components/Home.vue";
 
 export const routes = [
-  { path: "", component: Home },
+  {
+    path: "",
+    components: {
+      default: Home,
+      "header-top": Header
+    }
+  },
   {
     path: "/user",
-    component: User,
+    components: {
+      default: User,
+      "header-bottom": Header
+    },
     children: [
       { path: "", component: UserStart },
       { path: ":id", component: UserDetail },
