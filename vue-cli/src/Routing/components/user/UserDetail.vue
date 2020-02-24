4036de110b4232d28894c088077c663132f7ce73
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-24 11:56:54 +0600

    passing query params

--- vue-cli/src/Routing/components/user/UserDetail.vue
+++ vue-cli/src/Routing/components/user/UserDetail.vue
@@ -5,8 +5,7 @@
     <router-link
       tag="button"
       class="btn btn-primary"
-      :to="{ name: 'editUser', params: { id: $route.params.id } }"
-      >Edit User</router-link
-    >
+      :to="{ name: 'editUser', params: { id: $route.params.id } ,query:{locale:'en',q:100}}"
+    >Edit User</router-link>
   </div>
 </template>
--- vue-cli/src/Routing/components/user/UserEdit.vue
+++ vue-cli/src/Routing/components/user/UserEdit.vue
@@ -1,3 +1,7 @@
 <template>
-  <h3>Edit the User</h3>
+  <div clas="userEdit">
+    <h3>Edit the User</h3>
+    <p>Locale: {{ $route.query.locale }}</p>
+    <p>Analytics: {{ $route.query.q }}</p>
+  </div>
 </template>
