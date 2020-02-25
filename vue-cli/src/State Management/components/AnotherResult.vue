8673fdd23d25130747b08ffc1675badb0aa0b791
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-25 18:21:25 +0600

    using mapGetters

--- vue-cli/src/State Management/components/AnotherResult.vue
+++ vue-cli/src/State Management/components/AnotherResult.vue
@@ -1,13 +1,20 @@
 <template>
-  <p>Counter is: {{ counter }}</p>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Counter is: {{ stringCounter }}</p>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
-  computed: {
-    counter() {
-      return this.$store.getters.doubleCounter;
-    }
-  }
+  computed: mapGetters(["doubleCounter", "stringCounter"])
+
+  // counter() {
+  //   return this.$store.getters.doubleCounter;
+  // },
+  // stringCounter() {
+  //   return this.$store.getters.stringCounter;
+  // }
 };
 </script>
