caa22918e17296cfd36d471d9c360234aa906efb
NahiyanNashrah <nahiyan12@student.sust.edu>
2020-02-25 17:37:15 +0600

    State management with vuex

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,33 +1,39 @@
 import Vue from "vue";
+import App from "./State Management/App.vue";
 
-//    Routing
-import App from "./Routing/App.vue";
-import VueRouter from "vue-router";
-import { routes } from "./Routing/router";
-Vue.use(VueRouter);
-const router = new VueRouter({
-  routes: routes,
-  mode: "history",
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    if (to.hash) {
-      return { selector: to.hash };
-    }
-    return { x: 0, y: 0 };
-  }
-});
-router.beforeEach((to, from, next) => {
-  // to and from are both route objects. must call `next`.
-  console.log("before Each");
-  next();
-});
 new Vue({
   el: "#app",
-  router: router,
   render: h => h(App)
 });
+
+//    Routing
+// import App from "./Routing/App.vue";
+// import VueRouter from "vue-router";
+// import { routes } from "./Routing/router";
+// Vue.use(VueRouter);
+// const router = new VueRouter({
+//   routes: routes,
+//   mode: "history",
+//   scrollBehavior(to, from, savedPosition) {
+//     if (savedPosition) {
+//       return savedPosition;
+//     }
+//     if (to.hash) {
+//       return { selector: to.hash };
+//     }
+//     return { x: 0, y: 0 };
+//   }
+// });
+// router.beforeEach((to, from, next) => {
+//   // to and from are both route objects. must call `next`.
+//   console.log("before Each");
+//   next();
+// });
+// new Vue({
+//   el: "#app",
+//   router: router,
+//   render: h => h(App)
+// });
 
 //    Axios
 // import axios from "axios";
